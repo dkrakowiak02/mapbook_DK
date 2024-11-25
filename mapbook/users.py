06491c4a8ba4d8e0b378bcbb32f7f4ad35c3ab91
DZIAAAD <szymon.szperka@student.wat.edu.pl>
2024-11-25 14:47:56 +0100

    chore: add new users

--- mapbook/users.py
+++ mapbook/users.py
@@ -1,6 +1,6 @@
 users: list = [
     {'Name': 'Dominik', 'posts': 1, 'city': 'Poznań'},
-    {'Name': 'Dominik', 'posts': 3, 'city': 'Warszawa'},
+    {'Name': 'Patrycja', 'posts': 3, 'city': 'Warszawa'},
     {'Name': 'Szymon z wąsem', 'posts': 6, 'city': 'Inowrocław'},
     {'Name': 'Szymon', 'posts': 7, 'city': 'Białogard'},
     {'Name': 'Żerom', 'posts': 9, 'city': 'Tarnobrzeg'},
@@ -8,5 +8,10 @@ users: list = [
     {'Name': 'Dominik', 'posts': 1000, 'city': 'Siedlce'},
     {'Name': 'Kinga', 'posts': 3, 'city': 'Gdynia'},
     {'Name': 'Amelia', 'posts': 11, 'city': 'Toruń'},
-    {'Name': 'Karolina', 'posts': 5, 'city': 'Mława'},
+    {'Name': 'Julia', 'posts': 5, 'city': 'Zamość'},
+    {'Name': 'Aleksandra', 'posts': 4, 'city': 'Radom'},
+    {'Name': 'Patrycja', 'posts': 7, 'city': 'Mińsk Mazowiecki'},
+    {'Name': 'Patrycja', 'posts': 15, 'city': 'Kraśnik'},
+    {'Name': 'Patryk', 'posts': 20, 'city': 'Łódź'},
+    {'Name': 'Karolina', 'posts': 15, 'city': 'Mława'},
 ]
