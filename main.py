16a1325270b9b9f74768904895e759061d389f73
dkrakowiak02 <dominik.krakowiak@student.wat.edu.pl>
2024-12-02 12:58:53 +0100

    feat: add create new user

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from mapbook.users import users
-from mapbook.crud import hello,read_users
+from mapbook.crud import hello,read_users, add_user
+
+
 
 def main():
      hello(users[0]['Name'])
      read_users(users)
+     add_user(users)
 
 if __name__ == '__main__':
      main()
+
 
 
 
--- mapbook/crud.py
+++ mapbook/crud.py
@@ -5,3 +5,11 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['Name']}, {user['city']}, opublikował {user['posts']} postów')
+
+
+def add_user(userlist:list)-> None:
+    new_Name: str = input('Proszę podać imię nowego znajomego ')
+    new_posts: int = int(input ('Prosze podać liczbę postów '))
+    new_city: str = input ('Proszę podać miasto ')
+    new_user: dict = {'Name': new_Name, 'posts': new_posts, 'city': new_city}
+    userlist.append(new_user)
