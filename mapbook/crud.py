269c04fa2b40656e98edae451407d38f7ffc9061
dkrakowiak02 <dominik.krakowiak@student.wat.edu.pl>
2024-12-02 13:05:15 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -6,7 +6,7 @@ def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['Name']}, {user['city']}, opublikował {user['posts']} postów')
 
-add_user(users)
+
 def add_user(userlist:list)-> None:
     new_Name: str = input('Proszę podać imię nowego znajomego ')
     new_posts: int = int(input ('Prosze podać liczbę postów '))
